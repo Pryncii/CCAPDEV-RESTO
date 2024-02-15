d9c56896d827c39f3b8a8b4b26778d469e543bb0
m-chua <micolechuaten27@gmail.com>
2024-02-15 20:37:30 +0800

    Uploaded functions

--- functions.js
+++ functions.js
@@ -67,3 +67,46 @@ function toggleVisibility(ID) {
   
   }
   
+
+
+  
+
+function toggleVisibility(ID, button) {
+  var dropdown = document.getElementById(ID); 
+  var press =  document.getElementById(button); 
+  if(dropdown.style.display == "inline-block") { // if is menuBox displayed, hide it
+    dropdown.style.display = "none";
+    press.style.backgroundColor = "seagreen";
+  }
+  else { // if is menuBox hidden, display it
+    dropdown.style.display = "inline-block";
+    press.style.backgroundColor = "#037c07";
+  }
+}
+
+/*menu button */
+function toggleMenu() {
+  var menuBox = document.getElementById('menu');    
+  if(menuBox.style.display == "inline-block") { // if is menuBox displayed, hide it
+    menuBox.style.display = "none";
+  }
+  else { // if is menuBox hidden, display it
+    menuBox.style.display = "inline-block";
+  }
+}
+
+/*change color of tags upon select*/
+function select(id) {
+
+var div = document.getElementById(id);
+
+if(div.classList.contains("tags")  )
+{
+div.classList.replace("tags", "tags-selected");
+}
+else{
+    
+div.classList.replace("tags-selected", "tags");
+}
+
+}
