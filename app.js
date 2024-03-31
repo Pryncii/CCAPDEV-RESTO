9b2d9ddbee9022162e47b84c7ad4b21aef26122a
m-chua <micolechuaten27@gmail.com>
2024-03-31 22:29:49 +0800

    Merge branch 'main' of https://github.com/Pryncii/CCAPDEV-RESTO

--- app.js
+++ app.js
@@ -84,8 +84,9 @@ const restoSchema = new mongoose.Schema({
 },{ versionKey: false });
 
 const friendSchema = new mongoose.Schema({
-    friendname: { type: String },
-    friendimage: { type: String },
+    otherusername: { type: String },
+    otheruserimage: { type: String },
+    urlname: { type: String }
 });
 
 const userSchema = new mongoose.Schema({
@@ -95,7 +96,7 @@ const userSchema = new mongoose.Schema({
     pass: { type: String },
     image: { type: String },
     description: {type: String},
-    friends: [friendSchema],
+    otherusers: [friendSchema],
     revdata: [reviewSchema]
 },{ versionKey: false });
 
@@ -377,6 +378,17 @@ server.get('/restaurant/:landmark/:linkname', function(req, resp){
                 landmarkresto.push(restodata[i]);
             }
         }
+
+        let getratesum = 0;
+        for(let j = 0; j < restos.revdata.length; j++){
+            for(let k = 0; k < restos.revdata[j].revrating.length; k++){
+                if(restos.revdata[j].revrating[k] == "★"){
+                    getratesum+= 1;
+                }
+            }
+        }
+
+        console.log("rating:"+(getratesum/restos.revdata.length));
         
         resp.render('restopage',{
             layout      : 'index',
@@ -385,8 +397,7 @@ server.get('/restaurant/:landmark/:linkname', function(req, resp){
             otherresto  : landmarkresto,
             user        : loggedInUser,
             checkUser: isUser,
-
-            vrating      : 100-((restosJson.rating/5)*100)
+            vrating      : 100-(((getratesum/restos.revdata.length)/5)*100),
         });
     }
     }).catch(errorFn);
