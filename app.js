75fe3685a40bfce4c29fa708730bd298dc576483
Pryncii <princebuencamino@gmail.com>
2024-04-04 22:30:06 +0800

    woopa oopose

--- app.js
+++ app.js
@@ -147,11 +147,16 @@ var sresto = {
   R_Square:[],
   Man_Res:[]
 }
+
 restoModel.find({}).then(function(aresto){
 for(r of aresto){
   sresto[r.landmark].push({linkname: r.linkname, name: r.name});
 }
 });
+
+//Use this to determine the user who's logged in
+let loggedInUser;
+let isUser;
 
 server.get('/', function(req, resp){
     resp.render('main', {
@@ -174,10 +179,6 @@ if (req.session.login_user && req.session.login_id) {
 }
 })
 
-//Use this to determine the user who's logged in
-let loggedInUser;
-let isUser;
-
 server.get('/login-page', function(req, resp){
   const today = new Date();
  if(req.session.login_id == undefined || req.session.expiry <= today){
@@ -189,7 +190,6 @@ server.get('/login-page', function(req, resp){
   } else if(req.session.login_id && req.session.expiry > today) {
     userModel.findOne({ _id: req.session.login_user }).then(function (userfound) {
     if(userfound){
-      loggedInUser = userfound;
       resp.render('login',{
         layout      : 'index',
         title       : 'Login',
@@ -200,7 +200,6 @@ server.get('/login-page', function(req, resp){
     });
     } else {
       restoModel.findOne({ _id: req.session.login_user }).lean().then(function (restofound) {
-      loggedInUser = restofound;
       resp.render('login',{
         layout      : 'index',
         title       : 'Login',
